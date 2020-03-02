a9a7cbc75725cda2e5fa7813299b9369dd4cbaeb
Cc-qm <2827331792@qq.com>
2020-03-02 22:31:25 +0800

    登录注册功能的实现
    主要是写了登录注册的页面，以及实现了他们的功能，利用插件做了简单的检验，与mysql数据库交互，创建表，插入数据等操作。

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /*
- * @Author: cai 
- * @Date: 2020-03-01 12:28:44 
+ * @Author: cai
+ * @Date: 2020-03-01 12:28:44
  * @Last Modified by: cai
- * @Last Modified time: 2020-03-01 14:35:32
+ * @Last Modified time: 2020-03-02 20:51:33
  */
 
 //导入各个模块
@@ -17,68 +17,85 @@ const webserver = require("gulp-webserver");
 
 //定义 打包压缩各个文件 的函数
 const cssHandler = () => {
-    return gulp.src("./src/css/*.css")
-        .pipe(autoprefixer())
-        .pipe(cssmin())
-        .pipe(gulp.dest("./dist/css"));
-}
+  return gulp
+    .src("./src/css/*.css")
+    .pipe(autoprefixer())
+    .pipe(cssmin())
+    .pipe(gulp.dest("./dist/css"));
+};
 
 const jsHandler = () => {
-    return gulp.src("./src/js/*.js")
-        .pipe(babel({
-            presets: ["@babel/env"]
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"));
-}
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+};
 
 const htmlHandler = () => {
-    return gulp.src("./src/pages/*.html")
-        .pipe(htmlmin({
-            removeAttributeQuotes: true, //移除属性上的双引号
-            removeComments: true, //移除注释
-            collapseBooleanAttributes: true, //把布尔值属性简写
-            collapseWhitespace: true, //移除所有的空格
-            minifyCSS: true, //压缩内联的css代码
-            minifyJS: true //压缩script标签里面的js代码
-        }))
-        .pipe(gulp.dest("./dist/pages"));
-}
+  return gulp
+    .src("./src/pages/*.html")
+    .pipe(
+      htmlmin({
+        removeAttributeQuotes: true, //移除属性上的双引号
+        removeComments: true, //移除注释
+        collapseBooleanAttributes: true, //把布尔值属性简写
+        collapseWhitespace: true, //移除所有的空格
+        minifyCSS: true, //压缩内联的css代码
+        minifyJS: true //压缩script标签里面的js代码
+      })
+    )
+    .pipe(gulp.dest("./dist/pages"));
+};
 
 const imaHandler = () => {
-    return gulp.src("./src/images/**")
-        .pipe(gulp.dest("./dist/images"));
-}
+  return gulp.src("./src/images/**").pipe(gulp.dest("./dist/images"));
+};
 
 const libHandler = () => {
-    return gulp.src("./src/lib/**").pipe(gulp.dest("./dist/lib"));
+  return gulp.src("./src/lib/**").pipe(gulp.dest("./dist/lib"));
 };
 
 const delHandler = () => {
-    return del(["./dist"]);
-}
+  return del(["./dist"]);
+};
 
 const serverHandler = () => {
-    return gulp.src("./dist")
-        .pipe(webserver({
-            host: "localhost",
-            port: 8080,
-            open: "./pages/index.html",
-            livereload: true,
-        }))
-}
+  return gulp.src("./dist").pipe(
+    webserver({
+      host: "localhost",
+      port: 8080,
+      open: "./pages/index.html",
+      livereload: true,
+      proxies: [
+        {
+          source: "/login",
+          target: "http://localhost:8083/homework/shop_server/login.php"
+        },
+        {
+          source: "/register",
+          target: "http://localhost:8083/homework/shop_server/register.php"
+        }
+      ]
+    })
+  );
+};
 
 const watchHandler = () => {
-    gulp.watch("./src/css/*.css", cssHandler);
-    gulp.watch("./src/js/*.js", jsHandler);
-    gulp.watch("./src/pages/*.html", htmlHandler);
-    gulp.watch("./src/lib/**", libHandler);
-    gulp.watch("./src/images/**", imaHandler);
-}
+  gulp.watch("./src/css/*.css", cssHandler);
+  gulp.watch("./src/js/*.js", jsHandler);
+  gulp.watch("./src/pages/*.html", htmlHandler);
+  gulp.watch("./src/lib/**", libHandler);
+  gulp.watch("./src/images/**", imaHandler);
+};
 
 module.exports.default = gulp.series(
-    delHandler,
-    gulp.parallel(cssHandler, jsHandler, htmlHandler, imaHandler, libHandler),
-    serverHandler,
-    watchHandler
-)
+  delHandler,
+  gulp.parallel(cssHandler, jsHandler, htmlHandler, imaHandler, libHandler),
+  serverHandler,
+  watchHandler
+);
--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,21 @@
 $(() => {
-  $("form").validate({
+  $(".go-register").click(function(e) {
+    e = e || window.event;
+    e.preventDefault();
+    $(".load-panel")
+      .hide()
+      .next()
+      .show();
+  });
+  $(".go-load").click(function(e) {
+    e = e || window.event;
+    e.preventDefault();
+    $(".register-panel")
+      .hide()
+      .prev()
+      .show();
+  });
+  $(".login").validate({
     rules: {
       username: "required",
       password: "required"
@@ -9,15 +25,75 @@ $(() => {
       password: "请输入您的密码"
     },
     submitHandler: function(form) {
-      $.post(
-        "url",
-        $(form).serialize(),
-        function(data, textStatus, jqXHR) {
-          console.log(data, textStatus);
-        },
-        "json"
-      );
+      $.ajax({
+        type: "post",
+        url: "/login",
+        data: $(form).serialize(),
+        dataType: "json",
+        success: function(response) {
+          console.log(response);
+          if (response.status=="success") {
+            alert("登录成功！");
+            // location.href = "./index.html";
+          } else {
+            alert("账号或密码错误，请重新填写或者去注册！");
+          }
+        }
+      });
     }
   });
-  console.log(111);
+
+  jQuery.validator.addMethod(
+    "isMobile",
+    function(value, element) {
+      var length = value.length;
+      var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+      return this.optional(element) || (length == 11 && mobile.test(value));
+    },
+    "请正确填写手机号码"
+  );
+
+  $(".register").validate({
+    rules: {
+      ID: "required",
+      name: "required",
+      password: {
+        required: true,
+        rangelength: [8, 16]
+        // checkPassStrength:true
+      },
+      password2: {
+        equalTo: "#password"
+      },
+      phone: "isMobile",
+      email: "email"
+    },
+    messages: {
+      ID: "该项为必填项！",
+      name: "该项为必填项！",
+      password: {
+        required: "该项为必填项！"
+      },
+      password2: "两次输入的密码不一致！"
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "post",
+        url: "/register",
+        data: $(form).serialize(),
+        dataType: "json",
+        success: function(response) {
+          if (response.status) {
+            alert("注册成功，请登录！");
+            $(".register-panel")
+              .hide()
+              .prev()
+              .show();
+          } else {
+            alert("该ID已存在，请更改ID，重新注册！");
+          }
+        }
+      });
+    }
+  });
 });
